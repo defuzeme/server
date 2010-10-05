816725ada4728492651b83f23c165c7a410bd868
Adrien Jarthon <bigbourin@rootbox.fr>
2010-10-05 14:09:41 +0200

    production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,13 @@ DefuzeMe::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.free.fr",
+    :port                 => 587,
+    :domain               => 'defuze.me',
+    :enable_starttls_auto => true  }
+
 
   # Enable threaded mode
   # config.threadsafe!
