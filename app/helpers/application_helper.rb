461a81289fc06fd68d2aef072bd8b972c6dd48cd
Adrien Jarthon <bigbourin@BigBecane.(none)>
2010-10-06 21:50:10 +0200

    new form layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
-
+  def auto_text_field f, field
+    out = ''
+    out += f.label field
+    out += f.text_field field
+    out += f.error_message_on field
+    out.html_safe
+  end
+  
+  def auto_password_field f, field
+    out = ''
+    out += f.label field
+    out += f.password_field field
+    out += f.error_message_on field
+    out.html_safe
+  end
 end
