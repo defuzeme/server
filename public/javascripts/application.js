bbe13f0e3f273ffbbd4360cd4e3ead0b243a8693
Adrien Jarthon <bigbourin@BigBecane.(none)>
2010-10-06 19:16:27 +0200

    #120 flash alert

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+window.addEvent('domready', function() {
+  Alertbox.inject($('content'), 'top')
+  window._alert = window.alert
+  window.alert = function(message) { Alertbox.display(message) }
+  $$('.flash').each(function(f) {
+    f.removeClass('flash')
+    Alertbox.display(f.get('text'), f.get('class'))
+    f.dispose()
+  })
+})
