b8bac13973fd253c47a283a4dee7960a80550bf2
Adrien Jarthon <bigbourin@gmail.com>
2010-12-12 15:02:59 +0100

    admin accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,20 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-u = User.create!(:login => 'bigbourin', :email => 'bigbourin@gmail.com',
-      :first_name => 'Adrien', :last_name => 'Jarthon', :password => 'jambon',
-      :password_confirmation => 'jambon', :invitation_code => 'd#ve7aS4')
-u.admin = true
-u.activate!
+puts "Creating admin accounts"
 
+def create_admin login, email, first_name, last_name
+  u = User.create!(:login => login, :email => email,
+        :first_name => first_name, :last_name => last_name, :password => 'd#ve7aS4',
+        :password_confirmation => 'd#ve7aS4', :invitation_code => 'd#ve7aS4')
+  u.admin = true
+  u.activate!
+end
+
+create_admin 'bigbourin', 'bigbourin@gmail.com',      'Adrien',   'Jarthon'
+create_admin 'dreewoo',   'dreewoo@gmail.com',        'Jocelyn',  'De La Rosa'
+create_admin 'arnoo',     'arnoo.sel@gmail.com',      'Arnaud',   'Sellier'
+create_admin 'athena',    'athylynna@gmail.com',      'Athena',   'Calmettes'
+create_admin 'luc',       'luc.peres.88@gmail.com',   'Luc',      'Peres'
+create_admin 'GaYa',      'gailla.fr@gmail.com',      'Francois', 'Gaillard'
+create_admin 'greys',     'moore.alexandre@gmail.com','Alexandre','Moore'
