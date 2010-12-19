4c614b61ba0cf426361d274bf31bcb6fd133f9d9
Adrien Jarthon <bigbourin@gmail.com>
2010-12-19 14:07:04 +0100

    exception notifier

---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,5 @@
+DefuzeMe::Application.config.middleware.use ExceptionNotifier,
+  :email_prefix => "[defuze.me exception] ",
+  :sender_address => %{"notifier" <notifier@defuze.me>},
+  :exception_recipients => %w{exceptions@defuze.me}
+
