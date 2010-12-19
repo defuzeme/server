fd4927471b71ea50d6440575f08d6d7526da7f76
Adrien Jarthon <bigbourin@gmail.com>
2010-12-19 14:18:37 +0100

    exception notifier

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,5 +1,6 @@
-DefuzeMe::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[defuze.me exception] ",
-  :sender_address => %{"notifier" <notifier@defuze.me>},
-  :exception_recipients => %w{exceptions@defuze.me}
-
+if Rails.env.production?
+  DefuzeMe::Application.config.middleware.use ExceptionNotifier,
+    :email_prefix => "[defuze.me exception] ",
+    :sender_address => %{"notifier" <notifier@defuze.me>},
+    :exception_recipients => %w{exceptions@defuze.me}
+end
