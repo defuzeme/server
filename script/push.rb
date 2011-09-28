d5574e86aa037549e781b00a63ae24e7f1fd7643
Adrien Jarthon <bigbourin@gmail.com>
2011-09-28 11:41:47 +0200

    Push server update

--- script/push.rb
+++ script/push.rb
@@ -1,21 +1,25 @@
 require 'rubygems'
 require 'em-websocket'
 
-@channel = EM::Channel.new
+@channels = {}
 
 EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
   ws.onopen do
+    radio = ws.request['query']['radio'].to_i if ws.request['query']['radio']
+    # Create or fetch channel
+    channel = (@channels[radio] ||= EM::Channel.new)
+
     # Handle new client
-    sid = @channel.subscribe { |msg| ws.send msg }
+    sid = channel.subscribe { |msg| ws.send msg }
 
     # Send messages to all clients
     ws.onmessage do |msg|
-      @channel.push msg
+      channel.push msg
     end
 
     # Remove from channel if disconnected
     ws.onclose do
-      @channel.unsubscribe(sid)
+      channel.unsubscribe(sid)
     end
     
     # Display server-side errors
