219608f651fb877012f380bdb5799a347044eb2d
Adrien Jarthon <jobs@adrienjarthon.com>
2012-02-23 12:01:24 +0100

    #360 jQuery flash msg

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-// TODO: rewrite in jQuery
-
-/*
-window.addEvent('domready', function() {
-  $$('.flash').each(function(f) {
-    close = new Element('a', {'class': 'close', text: 'close', href: ''})
-    close.inject(f)
-    close.addEvent('click', function(e) {
-      e.stop()
-      f.dispose()
-      return false
-    })
-  })
-})*/
-
 // Audio play control related
 function play(playBtn) {
   pauseBtn = $(playBtn).next();
@@ -45,6 +30,20 @@ function sendControl(action) {
 
 // Domready
 $(document).ready(function() {
+  
+  // Flash messages
+  $('.flash').each(function(i, f) {
+    close = $(f).append("<a class='close' href='#'>close</a>");
+    window.setTimeout(function() {
+      $(f).fadeOut(2000);
+    }, 6000);
+    close.click(function() {
+      $(f).fadeOut(300);
+      return false;
+    })
+  })
+
+  // Queue sync
   var queue = $("ol.queue");
   if (queue.length)
   {
@@ -79,6 +78,7 @@ $(document).ready(function() {
     }
   };
   
+  // Sortable queue + ajax
   queue.sortable({
     placeholder: "ui-placeholder",
     update: function(event, ui) {
